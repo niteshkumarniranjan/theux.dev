a81edf423421854b4ed8665e2e8f240991193c53
Nitesh Kumar Niranjan <hello@theux.dev>
2021-01-13 19:18:49 +0530

    fix: og image

--- components/Post.js
+++ components/Post.js
@@ -19,7 +19,7 @@ const Post = ({
   date,
 }) => {
   const api = 'https://i.microlink.io/';
-  const cardUrl = `https://cards.microlink.io/?preset=paco&title=${title.replace(' ', '+')}&logo=https%3A%2F%2Fsvgur.com%2Fi%2FT5t.svg&domain=https%3A%2F%2Ftheux.dev&p=2gJ3PEZsZXgKICBzeD17ewogICAgYWxpZ25JdGVtczogJ2ZsZXgtZW5kJywKICAgIGJnOiBxdWVyeS5iZywKICAgIGNvbG9yOiBxdWVyeS5jb2xvciwKICAgIHBhZGRpbmc6IDgwLAogIH19Cj4KICA8RmxleAogICAgc3g9e3sKICAgICAganVzdGlmeUNvbnRlbnQ6ICdzcGFjZS1iZXR3ZWVuJywKICAgICAgYWxpZ25JdGVtczogJ2ZsZXgtZW5kJywKICAgIH19CiAgPgogICAgPExpbmsKICAgICAgaHJlZj0naHR0cHM6Ly9mb250cy5nb29nbGVhcGlzLmNvbS9jc3MyP2ZhbWlseT1JbnRlcjp3Z2h0QDcwMCZkaXNwbGF5PXN3YXAnCiAgICAgIHJlbD0nc3R5bGVzaGVldCcKICAgIC8-CiAgICA8VGV4dAogICAgICBzeD17ewogICAgICAgIGZvbnRGYW1pbHk6ICdJbnRlcicsCiAgICAgICAgZm9udFNpemU6IDY3LAogICAgICAgIGZvbnRXZWlnaHQ6IDcwMCwKICAgICAgICBsaW5lSGVpZ2h0OiAnMTAwJScsCiAgICAgICAgbGV0dGVyU3BhY2luZzogJy00cHgnLAogICAgICAgIGZsZXg6ICcwIDEgNjAlJywKICAgICAgfX0KICAgID4KICAgICAge3F1ZXJ5LnRpdGxlfQogICAgPC9UZXh0PgogICAgPEltYWdlIHN0eWxlPXt7IHdpZHRoOiAnODBweCcgfX0gc3JjPXtxdWVyeS5sb2dvfSAvPgogIDwvRmxleD4KPC9GbGV4Pg`;
+  const cardUrl = `https://cards.microlink.io/?preset=paco&logo=https%3A%2F%2Fsvgshare.com%2Fi%2FT4R.svg&title=${title.replace(' ', '+')}`;
   const image = `${api}${encodeURIComponent(cardUrl)}`;
 
   return <div className="py-8 mx-auto max-w-7xl">
